376bdf997bc55a88dc3ddd84ae0e1a55b8f8aaa0
funzter <cavehive@protonmail.com>
2022-02-06 23:32:44 +0100

    init

---
+++ src/main/java/de/timesnake/library/waitinggames/games/JumpRun.java
@@ -0,0 +1,152 @@
+package de.timesnake.library.waitinggames.games;
+
+import de.timesnake.basic.bukkit.util.Server;
+import de.timesnake.basic.bukkit.util.chat.ChatColor;
+import de.timesnake.basic.bukkit.util.exceptions.WorldNotExistException;
+import de.timesnake.basic.bukkit.util.user.ExItemStack;
+import de.timesnake.basic.bukkit.util.user.User;
+import de.timesnake.basic.bukkit.util.user.event.UserInventoryInteractEvent;
+import de.timesnake.basic.bukkit.util.user.event.UserInventoryInteractListener;
+import de.timesnake.basic.bukkit.util.user.event.UserMoveEvent;
+import de.timesnake.basic.bukkit.util.user.event.UserQuitEvent;
+import de.timesnake.basic.bukkit.util.world.ExLocation;
+import de.timesnake.library.basic.util.Tuple;
+import de.timesnake.library.waitinggames.GameFile;
+import de.timesnake.library.waitinggames.GameLoadException;
+import de.timesnake.library.waitinggames.Plugin;
+import de.timesnake.library.waitinggames.WaitingGameManager;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+import java.time.Duration;
+
+public class JumpRun extends WaitingGame implements Listener, UserInventoryInteractListener {
+
+    public static final String NAME = "jump_run";
+
+    private static final String START = "start";
+    private static final String FINISH = "finish";
+
+    private final ExItemStack startItem = new ExItemStack(Material.WHITE_DYE, "§6Start");
+    private final ExItemStack leaveItem = new ExItemStack(8, Material.RED_DYE, "§cLeave");
+
+    private final ExLocation start;
+    private final ExLocation finish;
+
+    public JumpRun(ExLocation start, ExLocation finish) {
+        this.start = start;
+        this.finish = finish;
+
+        GameFile file = WaitingGameManager.getInstance().getGameFile(start.getExWorld());
+
+        super.id = file.addGame(NAME, new Tuple<>(START, start), new Tuple<>(FINISH, finish));
+
+        Server.registerListener(this);
+        Server.getInventoryEventManager().addInteractListener(this, this.startItem, this.leaveItem);
+    }
+
+    public JumpRun(GameFile file, int id) throws GameLoadException {
+        super(id);
+
+        if (!file.containsGame(id)) {
+            throw new GameLoadException(NAME, id);
+        }
+
+        Location startLoc = null;
+        Location finishLoc = null;
+
+        try {
+            startLoc = file.getLocationValue(id, START);
+            finishLoc = file.getLocationValue(id, FINISH);
+        } catch (WorldNotExistException ignored) {
+        }
+
+        if (startLoc == null || finishLoc == null) {
+            throw new GameLoadException(NAME, id);
+        }
+
+        this.start = new ExLocation(Server.getWorld(startLoc.getWorld()), startLoc.getX(), startLoc.getY(), startLoc.getZ());
+        this.finish = new ExLocation(Server.getWorld(finishLoc.getWorld()), finishLoc.getX(), finishLoc.getY(), finishLoc.getZ());
+
+        Server.registerListener(this);
+        Server.getInventoryEventManager().addInteractListener(this, this.startItem, this.leaveItem);
+    }
+
+    @EventHandler
+    public void onUserMove(UserMoveEvent e) {
+
+        User user = e.getUser();
+
+        if (e.getTo().getBlock().equals(this.start.getBlock())) {
+            if (this.users.contains(user)) {
+                return;
+            }
+
+            this.users.add(user);
+            user.sendTitle("§9Jump'n Run", "", Duration.ofSeconds(2));
+
+            this.inventoriesByUser.put(user, user.getInventory().getContents());
+            user.clearInventory();
+
+            user.setItem(3, startItem);
+            user.setItem(4, startItem);
+            user.setItem(5, startItem);
+            user.setItem(leaveItem);
+        }
+
+        if (!this.users.contains(user)) {
+            return;
+        }
+
+        if (e.getTo().getBlock().equals(this.finish.getBlock())) {
+            this.users.remove(user);
+            user.clearInventory();
+            user.getInventory().setContents(this.inventoriesByUser.remove(user));
+            user.sendTitle("§6Finished", "", Duration.ofSeconds(2));
+            Server.broadcastMessage(Plugin.WAITING_GAME, user.getChatName() + ChatColor.PUBLIC + " finished the Jump'n Run");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerTeleport(PlayerTeleportEvent e) {
+
+        User user = Server.getUser(e.getPlayer());
+
+        if (!this.users.contains(user)) {
+            return;
+        }
+
+        if (e.getCause().equals(PlayerTeleportEvent.TeleportCause.CHORUS_FRUIT) || e.getCause().equals(PlayerTeleportEvent.TeleportCause.ENDER_PEARL)) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onUserLeave(UserQuitEvent e) {
+        this.users.remove(e.getUser());
+    }
+
+    @Override
+    public void onUserInventoryInteract(UserInventoryInteractEvent event) {
+        ExItemStack clickedItem = event.getClickedItem();
+        User user = event.getUser();
+
+        if (!this.users.contains(user)) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        if (clickedItem.equals(this.startItem)) {
+            user.teleport(this.start);
+        } else if (clickedItem.equals(this.leaveItem)) {
+            this.users.remove(user);
+            user.clearInventory();
+            user.getInventory().setContents(this.inventoriesByUser.remove(user));
+            user.sendTitle("§cLeft", "", Duration.ofSeconds(2));
+        }
+    }
+}
