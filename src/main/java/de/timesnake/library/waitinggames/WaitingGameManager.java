c4acec980f8e4fce3b850c3ebbc3ea46234d9699
funzter <funzter@mail.timesnake.de>
2022-10-08 11:44:43 +0200

    added license and copyright

--- src/main/java/de/timesnake/library/waitinggames/WaitingGameManager.java
+++ src/main/java/de/timesnake/library/waitinggames/WaitingGameManager.java
@@ -1,3 +1,21 @@
+/*
+ * library-waiting-game.main
+ * Copyright (C) 2022 timesnake
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.timesnake.library.waitinggames;
 
 import de.timesnake.basic.bukkit.util.Server;
@@ -13,9 +31,14 @@ import java.util.*;
 
 public class WaitingGameManager {
 
+    public static WaitingGameManager getInstance() {
+        return instance;
+    }
+
     private static WaitingGameManager instance;
     private final HashMap<ExWorld, GameFile> gameFilesByWorld = new HashMap<>();
     private final HashMap<ExWorld, List<WaitingGame>> gamesByWorld = new HashMap<>();
+
     public WaitingGameManager() {
         instance = this;
 
@@ -64,10 +87,6 @@ public class WaitingGameManager {
         }
 
         Server.printText(Plugin.WAITING_GAME, "Loaded waiting game manager");
-    }
-
-    public static WaitingGameManager getInstance() {
-        return instance;
     }
 
     public GameFile getGameFile(ExWorld world) {
