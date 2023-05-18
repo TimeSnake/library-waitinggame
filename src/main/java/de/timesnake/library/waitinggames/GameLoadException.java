b9956a4f9066f58a952bd490567149e8ca4326ae
funzter <cavehive@protonmail.com>
2023-05-18 11:27:33 +0200

    reindented files with two spaces

--- src/main/java/de/timesnake/library/waitinggames/GameLoadException.java
+++ src/main/java/de/timesnake/library/waitinggames/GameLoadException.java
@@ -6,20 +6,20 @@ package de.timesnake.library.waitinggames;
 
 public class GameLoadException extends Exception {
 
-    private final String game;
-    private final int id;
+  private final String game;
+  private final int id;
 
-    public GameLoadException(String game, int id) {
-        super("Can not load " + game + " with id " + id);
-        this.game = game;
-        this.id = id;
-    }
+  public GameLoadException(String game, int id) {
+    super("Can not load " + game + " with id " + id);
+    this.game = game;
+    this.id = id;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public String getGame() {
-        return game;
-    }
+  public String getGame() {
+    return game;
+  }
 }
--- src/main/java/de/timesnake/library/waitinggames/Plugin.java
+++ src/main/java/de/timesnake/library/waitinggames/Plugin.java
@@ -5,15 +5,15 @@
 package de.timesnake.library.waitinggames;
 
 import de.timesnake.library.basic.util.LogHelper;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-    public static final Plugin WAITING_GAME = new Plugin("Games", "LWG", LogHelper.getLogger("Games", Level.INFO));
+  public static final Plugin WAITING_GAME = new Plugin("Games", "LWG",
+      LogHelper.getLogger("Games", Level.INFO));
 
-    protected Plugin(String name, String code, Logger logger) {
-        super(name, code, logger);
-    }
+  protected Plugin(String name, String code, Logger logger) {
+    super(name, code, logger);
+  }
 }
