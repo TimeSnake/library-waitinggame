5fdec1ba1de743592ef2e0f4a76425eda31510a4
funzter <funzter@mail.timesnake.de>
2025-01-13 19:29:36 +0100

    :alien: update for changed database api

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,8 +31,8 @@ repositories {
 dependencies {
     compileOnly("de.timesnake:basic-bukkit:4.+")
 
-    compileOnly("de.timesnake:database-bukkit:3.+")
-    compileOnly("de.timesnake:database-api:3.+")
+    compileOnly("de.timesnake:database-bukkit:4.+")
+    compileOnly("de.timesnake:database-api:4.+")
 
     compileOnly("de.timesnake:channel-bukkit:5.+")
     compileOnly("de.timesnake:channel-api:5.+")
